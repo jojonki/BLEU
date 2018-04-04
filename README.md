9837bb0d03b2c7d20177cade68383e60fec95068
jojonki <junki.ohmura@gmail.com>
2018-04-04 10:16:59 -0400

    run multiple N-grams

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # BLEU
+
+## How to use?
+
+```
+python modified_ngram_precision.py < tests/xxx.txt
+> P_1 = 0.9444444444444444 (17/18)
+> P_2 = 0.5882352941176471 (10/17)
+> P_3 = 0.4375 (7/16)
+```
--- modified_ngram_precision.py
+++ modified_ngram_precision.py
@@ -1,12 +1,7 @@
 import sys
 import copy
 
-# if len(sys.argv) < 2:
-#     print('python program.py <n-gram>')
-#     sys.exit()
 
-Ns = [1, 2, 3]
-N = 1
 mt = input()
 refs = []
 while True:
@@ -16,10 +11,10 @@ while True:
     except EOFError:
         break
 
-print('mt', mt)
-print('refs', refs)
+print('MT:', mt)
+print('Refs:', refs)
 
-mt = mt.split(' ')
+Ns = [1, 2, 3]
 
 
 def make_ngrams(sentence, N):
@@ -30,31 +25,31 @@ def make_ngrams(sentence, N):
     return ngrams
 
 
-mt_counter = {}
-for ngram in mt:
-    if ngram not in mt_counter:
-        mt_counter[ngram] = 1
-    else:
-        mt_counter[ngram] += 1
-
-ref_counters = []
-for ref in refs:
-    counter = {}
-    ngrams = make_ngrams(ref, N)
-    for ngram in make_ngrams(ref, N):
-        if ngram not in counter:
-            counter[ngram] = 1
+for N in Ns:
+    mt_counter = {}
+    for ngram in make_ngrams(mt, N):
+        if ngram not in mt_counter:
+            mt_counter[ngram] = 1
         else:
-            counter[ngram] += 1
-    ref_counters.append(copy.copy(counter))
+            mt_counter[ngram] += 1
 
-clip_count = 0
-for token, mt_count in mt_counter.items():
-    max_ref_count = 0
-    for ref_counter in ref_counters:
-        if token in ref_counter:
-            max_ref_count = max(ref_counter[token], max_ref_count)
-    print('token', token, mt_count, max_ref_count)
-    clip_count += min(mt_count, max_ref_count)
-total = len(mt)
-print('P_{} = {} ({}/{})'.format(N, clip_count/total, clip_count, total))
+    ref_counters = []
+    for ref in refs:
+        counter = {}
+        ngrams = make_ngrams(ref, N)
+        for ngram in make_ngrams(ref, N):
+            if ngram not in counter:
+                counter[ngram] = 1
+            else:
+                counter[ngram] += 1
+        ref_counters.append(copy.copy(counter))
+
+    clip_count = 0
+    for token, mt_count in mt_counter.items():
+        max_ref_count = 0
+        for ref_counter in ref_counters:
+            if token in ref_counter:
+                max_ref_count = max(ref_counter[token], max_ref_count)
+        clip_count += min(mt_count, max_ref_count)
+    total = sum([ct for ct in mt_counter.values()])
+    print('P_{} = {} ({}/{})'.format(N, clip_count/total, clip_count, total))
