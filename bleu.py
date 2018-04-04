7effc02357ac075859c2efd6b98268753f15eb5f
jojonki <junki.ohmura@gmail.com>
2018-04-04 16:24:07 -0400

    add smoothing function

--- bleu.py
+++ bleu.py
@@ -1,75 +1,82 @@
 import copy
 import math
+from fractions import Fraction
 
 
-class BLEU:
-    def __init__(self):
-        pass
+def make_ngrams(sentence, N):
+    ngrams = []
+    for i in range(len(sentence) - N + 1):
+        ngrams.append(' '.join(sentence[i:i+N]))
+    return ngrams
 
-    @staticmethod
-    def make_ngrams(sentence, N):
-        ngrams = []
-        for i in range(len(sentence) - N + 1):
-            ngrams.append(' '.join(sentence[i:i+N]))
-        return ngrams
 
-    @staticmethod
-    def modified_precision(mt, refs, maxN=4):
-        Ns = range(1, maxN)
-        Ps = []
-        for N in Ns:
-            mt_counter = {}
-            for ngram in BLEU.make_ngrams(mt, N):
-                if ngram not in mt_counter:
-                    mt_counter[ngram] = 1
+def modified_precision(mt, refs, maxN=4, weights=None, epsilon=0.1):
+    if weights is not None:
+        assert(maxN == len(weights))
+
+    Ns = range(1, maxN+1)
+    Ps = []
+    for N in Ns:
+        mt_counter = {}
+        for ngram in make_ngrams(mt, N):
+            if ngram not in mt_counter:
+                mt_counter[ngram] = 1
+            else:
+                mt_counter[ngram] += 1
+
+        ref_counters = []
+        for ref in refs:
+            counter = {}
+            for ngram in make_ngrams(ref, N):
+                if ngram not in counter:
+                    counter[ngram] = 1
                 else:
-                    mt_counter[ngram] += 1
+                    counter[ngram] += 1
+            ref_counters.append(copy.copy(counter))
 
-            ref_counters = []
-            for ref in refs:
-                counter = {}
-                for ngram in BLEU.make_ngrams(ref, N):
-                    if ngram not in counter:
-                        counter[ngram] = 1
-                    else:
-                        counter[ngram] += 1
-                ref_counters.append(copy.copy(counter))
+        clip_count = 0
+        for token, mt_count in mt_counter.items():
+            max_ref_count = 0
+            for ref_counter in ref_counters:
+                if token in ref_counter:
+                    max_ref_count = max(ref_counter[token], max_ref_count)
+            clip_count += min(mt_count, max_ref_count)
+        total = sum([ct for ct in mt_counter.values()])
+        Ps.append(Fraction(clip_count, total, _normalize=False))
+        # print('P_{} = {:.3f} ({}/{})'.format(N, Ps[-1], clip_count, total))
 
-            clip_count = 0
-            for token, mt_count in mt_counter.items():
-                max_ref_count = 0
-                for ref_counter in ref_counters:
-                    if token in ref_counter:
-                        max_ref_count = max(ref_counter[token], max_ref_count)
-                clip_count += min(mt_count, max_ref_count)
-            total = sum([ct for ct in mt_counter.values()])
-            Ps.append(clip_count/total)
-            # print('P_{} = {:.3f} ({}/{})'.format(N, Ps[-1], clip_count, total))
+    maxN = max(Ns)
+    if weights is None:
+        weights = [1 / maxN] * maxN
 
-        maxN = max(Ns)
-        # print('Total P = {:.3f}'.format(sum([math.log(p)/maxN for p in Ps])))
-        return sum([math.log(p)/maxN for p in Ps])
+    # smoothing. TODO use Fraction for better comparison
+    for i, p in enumerate(Ps):
+        if p.numerator == 0:
+            # print('###', epsilon , p.denominator)
+            Ps[i] = epsilon / p.denominator
 
-    @staticmethod
-    def closest_ref_length(c, ref_lens):
-        return min(ref_lens, key=lambda ref_len: (abs(ref_len - c), ref_len))
+    return math.fsum([weights[i] * math.log(p) for i, p in enumerate(Ps)])
 
-    @staticmethod
-    def brevity_penalty(mt, refs):
-        c = len(BLEU.make_ngrams(mt, 1))
-        ref_lens = [len(BLEU.make_ngrams(ref, 1)) for ref in refs]
-        r = BLEU.closest_ref_length(c, ref_lens)
-        if c >= r:
-            return 1.0
-        else:
-            return math.exp(1.0 - r/c)
 
-    @staticmethod
-    def calc_bleu(mt, refs, maxN=4):
-        # mt_ngrams = BLUE.make_ngrams(mt, maxN)
-        # refs_ngrams = [BLUE.make_ngrams(ref, maxN) for ref in refs]
-        bp = BLEU.brevity_penalty(mt, refs)
-        return bp * math.exp(BLEU.modified_precision(mt, refs, maxN))
+def closest_ref_length(c, ref_lens):
+    return min(ref_lens, key=lambda ref_len: (abs(ref_len - c), ref_len))
+
+
+def brevity_penalty(mt, refs):
+    c = len(make_ngrams(mt, 1))
+    ref_lens = [len(make_ngrams(ref, 1)) for ref in refs]
+    r = closest_ref_length(c, ref_lens)
+    if c >= r:
+        return 1.0
+    else:
+        return math.exp(1.0 - r/c)
+
+
+def calc_bleu(mt, refs, maxN=4, weights=None):
+    # mt_ngrams = BLUE.make_ngrams(mt, maxN)
+    # refs_ngrams = [BLUE.make_ngrams(ref, maxN) for ref in refs]
+    bp = brevity_penalty(mt, refs)
+    return bp * math.exp(modified_precision(mt, refs, maxN, weights))
 
 # hypothesis = ['a'] * 12
 # hyp_len = len(hypothesis)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import math
-from bleu import BLEU
+from bleu import brevity_penalty, modified_precision, calc_bleu
 
 
 mt = input().split(' ')
@@ -14,8 +14,25 @@ while True:
 print('MT:', mt)
 print('Refs:', refs)
 
-print('------RESULT------')
-print('modified p:',      BLEU.modified_precision(mt, refs))
-print('exp(modified p):', math.exp(BLEU.modified_precision(mt, refs)))
-print('brevity penalty:', BLEU.brevity_penalty(mt, refs))
-print('BLEU score:',      BLEU.calc_bleu(mt, refs))
+
+def test():
+    print('------RESULT------')
+    # print('modified p:',      modified_precision(mt, refs))
+    print('exp(modified p):', math.exp(modified_precision(mt, refs)))
+    print('brevity penalty:', brevity_penalty(mt, refs))
+    print('BLEU score:',      calc_bleu(mt, refs))
+
+
+def nltk_test():
+    print('------NLTK-------')
+    from nltk.translate.bleu_score import closest_ref_length, brevity_penalty, sentence_bleu, corpus_bleu, SmoothingFunction
+    # from nltk_bleu import closest_ref_length, brevity_penalty, sentence_bleu, corpus_bleu, SmoothingFunction # from local file
+    # nltk.translatel.bleu == sentence_bleu
+    sm_func = SmoothingFunction().method1
+    print('brevity_penalty:', brevity_penalty(closest_ref_length(refs, len(mt)), len(mt)))
+    print('sentence_bleu:',   sentence_bleu(refs, mt, smoothing_function=sm_func))
+    print('corpus_bleu:',     corpus_bleu([refs], [mt], smoothing_function=sm_func))
+
+
+test()
+nltk_test()
